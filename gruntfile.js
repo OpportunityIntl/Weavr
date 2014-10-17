8e8ed7e902c76adc9f8fbbfe27b470b1f05fd613
Joe Weber <wosephjeber@gmail.com>
2014-10-17 10:01:25 -0500

    Setting up grunt with sass and watch

---
+++ gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %> */\n',
+    
+    sass: {
+      dist: {
+        files: {
+          'style/style.css' : 'sass/style.scss'
+        }
+      }
+    }
+    
+  });
+
+  // Load plugins
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task(s).
+  grunt.registerTask('default', [ 'sass:dist' ] );
+
+};
