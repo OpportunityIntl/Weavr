c5afe3a75811c9be9780a5a068480138101544cd
Joe Weber <wosephjeber@gmail.com>
2015-12-07 09:17:03 -0600

    Update amount on keyup, dry up choose module js

--- dist/weavr/OI.weavr.js
+++ dist/weavr/OI.weavr.js
@@ -244,35 +244,39 @@ OI.layout.init();
     options.callback.call(_this, amount);
   }
   
+  function getAmount(input) {
+    var amount = Number(_this.sanitizeNumber(input.val())).toFixed(options.decimals);
+    return amount > 0 ? amount : null;
+  }
+  
   this.sanitizeNumber = function(value) {
     return value.replace(/[^0-9\.]/g, '');
   };
   
   this.formatNumber = function(value) {
     var re = '\\d(?=(\\d{3})+' + (options.decimals > 0 ? '\\.' : '$') + ')';
-    return value.replace(new RegExp(re, 'g'), '$&,');
+    return String(value).replace(new RegExp(re, 'g'), '$&,');
   };
   
   customAmountInput.on('focus', function() {
     $(this).addClass('selected');
     radios.filter(':checked').prop('checked', false);
-    updateTarget(_this.sanitizeNumber($(this).val()));
+    
+    updateTarget(getAmount($(this)));
+  });
+  
+  customAmountInput.on('keyup', function() {
+    updateTarget(getAmount($(this)));
   });
   
   customAmountInput.on('change', function() {
-    amount = Number(_this.sanitizeNumber($(this).val())).toFixed(options.decimals);
-    if (amount > 0) {
-      updateTarget(amount);
-      $(this).val('$' + _this.formatNumber(amount));
-    } else {
-      updateTarget(null);
-      $(this).val('');
-    }
+    var amount = getAmount($(this));
+    updateTarget(amount);
+    $(this).val(amount > 0 ? '$' + _this.formatNumber(amount) : '');
   });
   
   radios.on('click', function() {
     customAmountInput.removeClass('selected');
-    amount = $(this).val();
     updateTarget(_this.sanitizeNumber($(this).val()));
   });
 };
